e05a2d12c09e808ad82a80fd722412dc6d114df2
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-10-20 23:41:04 +0530

    Added-welcome to address book program

---
+++ src/com/bridgelabz/addressbook/Contacts.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbook;
+
+public class Contacts {
+	 private String firstname;
+	    private String lastname;
+	    private String Address;
+	    private String city;
+	    private String state;
+	    private Double Pin;
+	    private Double phonenumber;
+	    private String email;
+
+}
