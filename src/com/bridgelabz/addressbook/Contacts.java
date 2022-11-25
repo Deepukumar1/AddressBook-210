83f9003264f3d1f513c9269a9002377f0a003fc4
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-11-25 15:25:17 +0530

    Added: ability to read write using json file

--- src/com/bridgelabz/addressbook/Contacts.java
+++ src/com/bridgelabz/addressbook/Contacts.java
@@ -1,89 +1,89 @@
 package com.bridgelabz.addressbook;
 
 public class Contacts {
-	 private String firstname;
-	    private String lastname;
-	    private String Address;
-	    private String city;
-	    private String state;
-	    private String Pin;
-	    private String phonenumber;
-	    private String email;
+	private String firstname;
+    private String lastname;
+    private String Address;
+    private String city;
+    private String state;
+    private String Pin;
+    private String phonenumber;
+    private String email;
 
-	    public String getFirstname() {
-	        return firstname;
-	    }
+    public String getFirstname() {
+        return firstname;
+    }
 
-	    public void setFirstname(String firstname) {
-	        this.firstname = firstname;
-	    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-	    public String getLastname() {
-	        return lastname;
-	    }
+    public String getLastname() {
+        return lastname;
+    }
 
-	    public void setLastname(String lastname) {
-	        this.lastname = lastname;
-	    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
-	    public String getAddress() {
-	        return Address;
-	    }
+    public String getAddress() {
+        return Address;
+    }
 
-	    public void setAddress(String address) {
-	        Address = address;
-	    }
+    public void setAddress(String address) {
+        Address = address;
+    }
 
-	    public String getCity() {
-	        return city;
-	    }
+    public String getCity() {
+        return city;
+    }
 
-	    public void setCity(String city) {
-	        this.city = city;
-	    }
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	    public String getState() {
-	        return state;
-	    }
+    public String getState() {
+        return state;
+    }
 
-	    public void setState(String state) {
-	        this.state = state;
-	    }
+    public void setState(String state) {
+        this.state = state;
+    }
 
-	    public String getPin() {
-	        return Pin;
-	    }
+    public String getPin() {
+        return Pin;
+    }
 
-	    public void setPin(String pin) {
-	        Pin = pin;
-	    }
+    public void setPin(String pin) {
+        Pin = pin;
+    }
 
-	    public String getPhonenumber() {
-	        return phonenumber;
-	    }
+    public String getPhonenumber() {
+        return phonenumber;
+    }
 
-	    public void setPhonenumber(String phonenumber) {
-	        this.phonenumber = phonenumber;
-	    }
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
 
-	    public String getEmail() {
-	        return email;
-	    }
+    public String getEmail() {
+        return email;
+    }
 
-	    public void setEmail(String email) {
-	        this.email = email;
-	    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	    @Override
-	    public String toString() {
-	        return "Contacts Details :--" +"\n"+
-	                "Firstname : " + firstname +"\n"+
-	                "Lastname : " + lastname + '\n'+
-	                "Address : " + Address + '\n' +
-	                "City : " + city + '\n' +
-	                "State : " + state + '\n' +
-	                "Pin : " + Pin +'\n'+
-	                "PhoneNumber : " + phonenumber +'\n'+
-	                "Email : " + email +'\n';
-	    }
+    @Override
+    public String toString() {
+        return "Contacts Details :--" +"\n"+
+                "Firstname : " + firstname +"\n"+
+                "Lastname : " + lastname + '\n'+
+                "Address : " + Address + '\n' +
+                "City : " + city + '\n' +
+                "State : " + state + '\n' +
+                "Pin : " + Pin +'\n'+
+                "PhoneNumber : " + phonenumber +'\n'+
+                "Email : " + email +'\n';
+    }
 }
